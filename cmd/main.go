5f010a51189b7f9962e09c08f05e9f3accf00cbb
loxsman <chebotarev.ea@yahoo.com>
2024-07-27 12:53:56 +0300

    test 2

--- cmd/main.go
+++ cmd/main.go
@@ -6,43 +6,69 @@ type Month struct {
 	id     int
 	name   string
 	lenght int
+	lenghtOfYear int
 }
 
 type Day struct {
 	id            int
 	name          string
 	numberOfTusks int
+	idDay         int
 }
 
 // func calculateDay() string {
 // 	for i := 0; i <= 365; i++ {
-
 // 	}
 // }
 
-func printTable(arrMonth [12]Month) {
+func printMonth(arrMonth [12]Month) {
 	for _, p := range arrMonth {
-		fmt.Printf("%d. %s x with lenght(%d)\n", p.id, p.name, p.lenght)
+		fmt.Printf("%d. %s with lenght(%d)\n", p.id, p.name, p.lenght)
+	}
+}
+
+func controls(arrMonth [12]Month) {
+	fmt.Println("Chose month")
+	var i int
+	switch i {
+	case 1:
+
+	}
+}
+
+func printDays(arrMonth [12]Month, arrDay []string) {
+	for i := 1; i <= 12; i++ {
+		if i == arrMonth[i].id {
+			for j := 1; j <= arrMonth[i].lenght; j++ {
+				fmt.Printf("%d. %s curr tasks(%d)\n", j, arrDay[0], p.lenght)
+			}
+		}
 	}
 }
 
 func main() {
-	arrDay := make([]string, 7, 7)
-	arrDay = append(arrDay, "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+	arrDayOnWeek := make([]string, 7, 7)
+	arrDayOnWeek = append(arrDayOnWeek, "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
-	arrMonth := [...]Month{
-		{1, "January", 31},
-		{2, "February", 28},
-		{3, "March", 31},
-		{4, "April", 30},
-		{5, "May", 31},
-		{6, "June", 30},
-		{7, "July", 31},
-		{8, "August", 31},
-		{9, "September", 30},
-		{10, "October", 31},
-		{11, "November", 30},
-		{12, "December", 31},
+	arrMonth := [12]Month{
+		{1, "January", 31, 31},
+		{2, "February", 28, 59},
+		{3, "March", 31, 90},
+		{4, "April", 30, 120},
+		{5, "May", 31, 151},
+		{6, "June", 30, 181},
+		{7, "July", 31, 212},
+		{8, "August", 31, 243},
+		{9, "September", 30, 273},
+		{10, "October", 31, 304},
+		{11, "November", 30, 334},
+		{12, "December", 31, 365},
 	}
-	printTable(arrMonth)
+
+	arrDay := make([]Day, 365, 365)
+	countMonth := 1
+	for i := 1; i <= 365; i++{
+		arrDay = append(arrDay, {i})
+	}
+	printMonth(arrMonth)
 }
